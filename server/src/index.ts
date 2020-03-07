5456ff03fe092ffec0d23eb897a5b7bd5af686a4
Iandra Bedin <iandrabedin@gmail.com>
2020-03-07 01:09:16 +0100

    chore: create docker config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
 const app = express();
+const PORT = process.env.APP_SERVER_PORT;
 
 app.get("/", (req, res) => {
   return res.send("Hello world");
 });
 
-app.listen(3333);
+app.listen(PORT);
